1fbd7ff0f1ebf5e057f7e0234ef55cf1e4df7886
cadaeibfe <cadaeibfe@users.noreply.github.com>
2021-02-26 16:13:45 -0700

    add npc actions after talking

--- main.py
+++ main.py
@@ -9,24 +9,38 @@ class World:
     def __init__(self):
         self.tiles = [[1, 1, 1, 1, 1, 1, 1, 1, 1],
                       [1, 0, 0, 0, 0, 0, 0, 0, 1],
+                      [1, 0, 0, 1, 1, 1, 0, 0, 1],
+                      [1, 0, 0, 1, 4, 1, 0, 0, 1],
+                      [1, 0, 0, 1, 0, 1, 0, 0, 1],
                       [1, 0, 0, 0, 0, 0, 0, 0, 1],
                       [1, 0, 0, 0, 0, 0, 0, 0, 1],
-                      [1, 0, 0, 0, 0, 0, 0, 0, 1],
-                      [1, 0, 0, 0, 0, 0, 0, 0, 1],
-                      [1, 0, 0, 0, 0, 0, 0, 0, 1],
+                      [1, 1, 1, 0, 0, 0, 1, 1, 1],
+                      [1, 1, 0, 0, 0, 0, 0, 1, 1],
+                      [1, 1, 0, 1, 1, 1, 0, 1, 1],
+                      [1, 1, 0, 0, 0, 0, 0, 1, 1],
+                      [1, 1, 1, 1, 0, 1, 1, 1, 1],
+                      [1, 1, 1, 0, 0, 0, 1, 1, 1],
+                      [1, 1, 0, 0, 0, 0, 0, 1, 1],
+                      [1, 1, 0, 0, 0, 0, 0, 1, 1],
+                      [1, 1, 1, 0, 0, 0, 1, 1, 1],
+                      [1, 1, 1, 1, 0, 1, 1, 1, 1],
                       [1, 1, 1, 1, 1, 1, 1, 1, 1]]
         self.width = len(self.tiles[0])
         self.height = len(self.tiles)
 
         self.mobs = {}
 
-        self.tile_sheet = pg.Surface((320, 64))
+        self.tile_sheet = pg.Surface((384, 64))
         self.tile_sheet.set_colorkey((0, 0, 0))
         pg.draw.rect(self.tile_sheet, (255, 0, 0), (0, 0, TILE_SIZE, TILE_SIZE), 1)                 # out of bounds
-        pg.draw.rect(self.tile_sheet, (80, 80, 80), (TILE_SIZE, 0, TILE_SIZE, TILE_SIZE), 1)        # floor
-        pg.draw.rect(self.tile_sheet, (245, 245, 245), (2*TILE_SIZE, 0, TILE_SIZE, TILE_SIZE), 1)   # wall
+        pg.draw.rect(self.tile_sheet, (48, 48, 48), (TILE_SIZE, 0, TILE_SIZE, TILE_SIZE))        # floor
+        pg.draw.rect(self.tile_sheet, (156, 156, 156), (2*TILE_SIZE, 0, TILE_SIZE, TILE_SIZE))   # wall
+        pg.draw.rect(self.tile_sheet, (80, 80, 80), (2*TILE_SIZE, 0, TILE_SIZE, TILE_SIZE), 2)
         pg.draw.rect(self.tile_sheet, (0, 121, 241), (3*TILE_SIZE, 0, TILE_SIZE, TILE_SIZE))        # player
         pg.draw.rect(self.tile_sheet, (0, 228, 48), (4*TILE_SIZE, 0, TILE_SIZE, TILE_SIZE))         # archaeologist
+        pg.draw.rect(self.tile_sheet, (48, 48, 48), (5*TILE_SIZE, 0, TILE_SIZE, TILE_SIZE))        # down stairs
+        pg.draw.line(self.tile_sheet, (245, 245, 245), (5.25*TILE_SIZE, 10), (5.75*TILE_SIZE, 0.5*TILE_SIZE), 10)
+        pg.draw.line(self.tile_sheet, (245, 245, 245), (5.25*TILE_SIZE, TILE_SIZE-10), (5.75*TILE_SIZE, 0.5*TILE_SIZE), 10)
 
 
     def get_tile(self, x, y):
@@ -48,7 +62,7 @@ class World:
 
     def is_walkable(self, x, y):
         tile = self.get_tile(x, y)
-        return tile == 0
+        return tile == 0 or tile == 4
 
     def add_mob_at(self, mob, x, y):
         self.mobs[(x, y)] = mob
@@ -66,6 +80,8 @@ class Mob:
         self.x = x
         self.y = y
         self.ai = ai
+        if self.ai != None:
+            self.ai.owner = self
 
     def move(self, mx, my):
         newx = self.x + mx
@@ -87,10 +103,28 @@ class Mob:
 class ArchAi:
     def __init__(self, game):
         self.game = game
+        self.lines = [
+            ("Archaeologist:\nOh, you're here!\nThanks for helping me with my expedition.", self.move_to_stairs),
+            ("Archaeologist:\nThis style of architecture looks familiar...\nWe must go deeper!", self.move_downstairs)
+        ]
+        self.talk_index = 0
 
     def on_bump(self):
-        text = "Archaeologist:\nOh, you're here!\nThanks for helping me with my expedition."
-        self.game.talking_time(text)
+        line = self.lines[self.talk_index][0]
+        action = self.lines[self.talk_index][1]
+        self.game.talking_time(line, action)
+
+    def move_to_stairs(self):
+        newx, newy = (4, 4)
+        self.owner.world.on_mob_move(self.owner.x, self.owner.y, newx, newy)
+        self.owner.x = newx
+        self.owner.y = newy
+        self.talk_index = 1
+
+    def move_downstairs(self):
+        # just temporarily remove from the world until I make more levels
+        self.owner.world.mobs.pop((self.owner.x, self.owner.y))
+        self.talk_index = 2
 
 
 class State(Enum):
@@ -127,8 +161,8 @@ class Game:
     def new_game(self):
         self.state = State.PLAY
         self.world = World()
-        self.player = Mob(self.world, 3, 1, 1, None)
-        self.arch = Mob(self.world, 4, 3, 1, ArchAi(self))
+        self.player = Mob(self.world, 3, 4, 15, None)
+        self.arch = Mob(self.world, 4, 4, 11, ArchAi(self))
 
     def on_event(self, event):
         if self.state == State.PLAY:
@@ -146,6 +180,8 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_z:
                     self.state = State.PLAY
+                    if self.talk_action != None:
+                        self.talk_action()
 
     def on_update(self, dt):
         pass
@@ -178,9 +214,10 @@ class Game:
 
             pg.draw.rect(surf, (245, 245, 245), (talk_rect.right-30, talk_rect.bottom-30, 20, 20))
 
-    def talking_time(self, text):
+    def talking_time(self, text, action):
         self.state = State.TALK
         self.talk_lines = text.split('\n')
+        self.talk_action = action
 
 
 def draw_text(surf, font, text, x, y):
