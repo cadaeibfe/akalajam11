4045204473380519cf9d84a25497e526d3dba34b
cadaeibfe <cadaeibfe@users.noreply.github.com>
2021-02-28 08:30:12 -0700

    move sound and images into data folder

--- assets.py
+++ assets.py
@@ -11,23 +11,23 @@ class Assets:
         Assets.small_font = pg.font.Font("freesansbold.ttf", 20)
 
         # images
-        Assets.tile_sheet_small = pg.image.load("images/tile_sheet.png").convert()   # save original size image for ui icons
+        Assets.tile_sheet_small = pg.image.load("data/images/tile_sheet.png").convert()   # save original size image for ui icons
         Assets.tile_sheet = pg.transform.scale(Assets.tile_sheet_small, (Assets.tile_sheet_small.get_width()*3, Assets.tile_sheet_small.get_height()*3))
         Assets.tile_sheet_flipped = pg.transform.flip(Assets.tile_sheet, True, False)
 
-        Assets.title_image = pg.image.load("images/title.png").convert()
+        Assets.title_image = pg.image.load("data/images/title.png").convert()
         Assets.title_image = pg.transform.scale(Assets.title_image, (Assets.title_image.get_width()*5, Assets.title_image.get_height()*5))
 
         # sounds
-        Assets.step_sound = pg.mixer.Sound("sounds/Hit_Hurt5.wav")
-        Assets.bump_sound = pg.mixer.Sound("sounds/Hit_Hurt4.wav")
-        Assets.hit_sound = pg.mixer.Sound("sounds/Hit_Hurt22.wav")
-        Assets.up_stairs_sound = pg.mixer.Sound("sounds/Hit_Hurt3.wav")
-        Assets.powerup_sound = pg.mixer.Sound("sounds/Powerup6.wav")
-        Assets.heal_sound = pg.mixer.Sound("sounds/Powerup12.wav")
-        Assets.select_sound = pg.mixer.Sound("sounds/Blip_Select6.wav")
-        Assets.game_over_sound = pg.mixer.Sound("sounds/Randomize39.wav")
-        Assets.win_sound = pg.mixer.Sound("sounds/Powerup19.wav")
+        Assets.step_sound = pg.mixer.Sound("data/sounds/Hit_Hurt5.wav")
+        Assets.bump_sound = pg.mixer.Sound("data/sounds/Hit_Hurt4.wav")
+        Assets.hit_sound = pg.mixer.Sound("data/sounds/Hit_Hurt22.wav")
+        Assets.up_stairs_sound = pg.mixer.Sound("data/sounds/Hit_Hurt3.wav")
+        Assets.powerup_sound = pg.mixer.Sound("data/sounds/Powerup6.wav")
+        Assets.heal_sound = pg.mixer.Sound("data/sounds/Powerup12.wav")
+        Assets.select_sound = pg.mixer.Sound("data/sounds/Blip_Select6.wav")
+        Assets.game_over_sound = pg.mixer.Sound("data/sounds/Randomize39.wav")
+        Assets.win_sound = pg.mixer.Sound("data/sounds/Powerup19.wav")
 
     @staticmethod
     def get_tile_image(tile, flip_h=False):
