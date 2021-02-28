d0df6543dbfad877a4647e33efdebb98fea5fe87
cadaeibfe <cadaeibfe@users.noreply.github.com>
2021-02-28 11:13:34 -0700

    remove dead code

--- data/mobs.py
+++ data/mobs.py
@@ -243,30 +243,3 @@ class Bat(Mob):
         # Bats are harder so have higher chance to drop a potion
         if rng.random() < 0.4:
             self.world.add_item_at(Item(Tile.POTION), self.x, self.y)
-
-
-# class ArchAi:
-#     def __init__(self, game):
-#         self.game = game
-#         self.lines = [
-#             ("Archaeologist:\nOh, you're here!\nThanks for helping me with my expedition.", self.move_to_stairs),
-#             ("Archaeologist:\nThis style of architecture looks familiar...\nWe must go deeper!", self.move_downstairs)
-#         ]
-#         self.talk_index = 0
-
-#     def on_bump(self):
-#         line = self.lines[self.talk_index][0]
-#         action = self.lines[self.talk_index][1]
-#         self.game.talking_time(line, action)
-
-#     def move_to_stairs(self):
-#         newx, newy = (4, 4)
-#         self.owner.world.on_mob_move(self.owner.x, self.owner.y, newx, newy)
-#         self.owner.x = newx
-#         self.owner.y = newy
-#         self.talk_index = 1
-
-#     def move_downstairs(self):
-#         # just temporarily remove from the world until I make more levels
-#         self.owner.world.mobs.pop((self.owner.x, self.owner.y))
-#         self.talk_index = 2
